c5b58e72ebc74db04ea2b993e815a1dc4311ad8b
Fernando_Matos <matos.fernandomp@gmail.com>
2024-08-29 23:10:16 -0300

    Aula - Product insert update delete .

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true) // Garantir que método seja executado por completo dentro de uma transação.
     // readOnly = true é para garantir que não trave(locking) o BANCO DE DADOS na leitura.
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -41,18 +47,17 @@ public class ProductService {
 
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
-        Product entity = new Product();
-       // entity.setName(dto.getName());
-        entity = productRepository.save(entity);
+        Product entity = new Product(); // Instanciando um produto vazio
+        copyDtoToEntity(dto,entity); // Copiando os dados do DTO
+        entity = productRepository.save(entity); // Salvando no banco de dados
         return new ProductDTO(entity);
     }
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
-            Product entity = productRepository.getReferenceById(id);
-         //   entity.setName(dto.getName());
-          //  entity = productRepository.save(entity);
+            Product entity = productRepository.getReferenceById(id);//Instanciando o produto com o id
+            copyDtoToEntity(dto,entity); // Copiando os dados do DTO
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found");
@@ -72,5 +77,18 @@ public class ProductService {
         }
     }
 
+    private void copyDtoToEntity (ProductDTO dto, Product entity){
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setDate(dto.getDate());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
 
+        entity.getCategories().clear();//Limpar a lista de categorias
+        for (CategoryDTO catDto : dto.getCategories()){
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category); // Associar na lista de categorias cada categoria que busquei
+        }
+
+    }
 }
