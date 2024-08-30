529615a9a25bc82817eee0eff44e497e48a2a5f3
Fernando_Matos <matos.fernandomp@gmail.com>
2024-08-29 21:06:57 -0300

    Aula - Dados de auditoria .

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +16,13 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
 
     public Category() {
     }
@@ -39,6 +47,25 @@ public class Category implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
+
 
     @Override
     public boolean equals(Object o) {
