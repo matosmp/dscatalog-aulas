e7af461f33df25d453bba5ef52e64da2afa4c675
Fernando_Matos <matos.fernandomp@gmail.com>
2024-09-17 21:11:31 -0300

    Aula - Testando findAllPaged do ProductService.

--- dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
@@ -14,7 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
@@ -50,7 +53,7 @@ public class ProductServiceTests {
         **/
         Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
 
-        
+
         Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
 
         //Irá retornar um optional de product
@@ -69,6 +72,17 @@ public class ProductServiceTests {
         Mockito.doThrow(DataBaseException.class).when(repository).deleteById(dependentId);
 
     }
+
+    @Test
+    public void findAllPagedShouldReturnPage(){
+        Pageable pageable = PageRequest.of(0,10);
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        Assertions.assertNotNull(result);
+        Mockito.verify(repository).findAll(pageable);
+
+    }
+
     @Test
     public void deleteShouldDoNothingWhenIdExist() {
         Assertions.assertDoesNotThrow(() -> {
